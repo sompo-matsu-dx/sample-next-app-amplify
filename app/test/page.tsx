b1b8d0f8917825ddb2229c31c4e51c757ff5cba7
masatoshi.matsushita <masatoshi.matsushita@lab.sompo.io>
2023-06-01 14:38:21 +0900

    new app

---
+++ app/test/page.tsx
@@ -0,0 +1,5 @@
+const Test = () => {
+    return <div className="text-xs">Testページ</div>;
+  };
+   
+  export default Test;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    experimental: {
+        addDir: true,
+    },
+}
 
 module.exports = nextConfig
